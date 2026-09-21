55baf29bd49aab87380e114058e96370767f1b52
Nitu2610 <nikss26796@gmail.com>
2026-09-21 08:33:09 +0530

    removed invalid import.

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,38 @@
-
-import { motion } from "framer-motion";
-import { useState } from 'react'
-import { CustomCursor } from './components/CustomCursor'
-import { Navbar } from './components/Navbar'
-import { About } from './sections/About'
-import { Contact } from './sections/Contact'
-import { Experience } from './sections/Experience'
-import { Footer } from './sections/Footer'
-import { Hero } from './sections/Hero'
-import { Projects } from './sections/Projects'
-import { Skills } from './sections/Skills'
-import { IntroPageLoader } from './components/IntroPageLoader'
-
-
+import { useState } from "react";
+import { CustomCursor } from "./components/CustomCursor";
+import { Navbar } from "./components/Navbar";
+import { About } from "./sections/About";
+import { Contact } from "./sections/Contact";
+import { Experience } from "./sections/Experience";
+import { Footer } from "./sections/Footer";
+import { Hero } from "./sections/Hero";
+import { Projects } from "./sections/Projects";
+import { Skills } from "./sections/Skills";
+import { IntroPageLoader } from "./components/IntroPageLoader";
 
 function App() {
-const [introDone, setIntroDone]=useState(false);
-
+  const [introDone, setIntroDone] = useState(false);
 
   return (
-
     <>
-{!introDone && <IntroPageLoader onFinish={()=> setIntroDone(true)}/>}
+      {!introDone && <IntroPageLoader onFinish={() => setIntroDone(true)} />}
 
-  {introDone && (
+      {introDone && (
+        <div className="relative gradient text-white">
+          <CustomCursor />
 
- 
-
-    <div className="relative gradient text-white">
-      <CustomCursor/>
-
-    <Navbar/>
-    <Hero/>
-    <About/>
-      <Skills/>
-    <Projects/>
-    <Experience/>
-    <Contact/>
-    <Footer/>
-    </div>
-     )}
+          <Navbar />
+          <Hero />
+          <About />
+          <Skills />
+          <Projects />
+          <Experience />
+          <Contact />
+          <Footer />
+        </div>
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
